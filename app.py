88f863ee6c6f58628295cf4ca32e972d6ac15438
Ipsita <srivastavaipsita@outlook.com>
2025-08-17 22:38:50 +0530

    modified app.py

--- app.py
+++ app.py
@@ -1,7 +1,2 @@
-import random
-code = (random.randint(1, 100))
-print (code)
-if code % 2 == 0:
-    print ("code is even")
-else:
-    print("code is odd")
+#   Logical Operations
+print (3>1 and 5<1)
