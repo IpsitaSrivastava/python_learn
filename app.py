e3a4bc73f7e041dd33c37fc6bb9cfd408973e9eb
Ipsita <125792622+IpsitaSrivastava@users.noreply.github.com>
2025-08-17 23:01:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 #   Logical Operations
 print (3>1 and 5<1)
+
+#   check if username is not empty and the age is greater than or equal to 18
+username="ola"
+age=18
+print(bool(username) and age>=18)
