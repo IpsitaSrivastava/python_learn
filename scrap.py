026a934f36249bd3a1a1a0ce1c2f9c4ad177f635
Ipsita <srivastavaipsita@outlook.com>
2025-08-31 23:27:59 +0530

    Learned Lists

--- scrap.py
+++ scrap.py
@@ -1,13 +1,5 @@
-if __name__ == '__main__':
-    x = int(input())
-    y = int(input())
-    z = int(input())
-    n = int(input())
+fruits = ["apple", "banana", "cherry"]
+fruits = [fruit.capitalize()for fruit in fruits]
 
-    coordinates = [[i, j, k] 
-                   for i in range(x+1) 
-                   for j in range(y+1) 
-                   for k in range(z+1) 
-                   if (i + j + k) != n]
-
-    print(coordinates)
+x = 9
+print(f'The price is {x} dollars')
