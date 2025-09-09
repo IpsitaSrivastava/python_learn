36e57fe50f48b6b1e4e7fe6d90b2930c0a8aca40
Ipsita <srivastavaipsita@outlook.com>
2025-09-09 23:57:04 +0530

    continuity

--- myset.py
+++ myset.py
@@ -1,13 +1,10 @@
 thisset = {"apple", "banana", "cherry"}
-
 #Get the Length of a Set
 thisset = {"apple", "banana", "cherry"}
 print(len(thisset))
-
 #set() Constructor
 thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
 print(thisset)
-
 #Check if "banana" is not present in the set:
 thisset = {"apple", "banana", "cherry"}
 print("banana" not in thisset)
